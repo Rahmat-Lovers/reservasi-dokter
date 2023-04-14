56146353b7fe34656615913aa8dc3294fbb0a02c
salismazaya <salismazaya@gmail.com>
2023-04-14 10:38:18 +0700

    ✨ feat(Index.svelte): menambahkan link ke halaman register dan login 🎨 style(Index.svelte): mengubah tampilan button register dan login di halaman utama

--- frontend/src/routes/Index.svelte
+++ frontend/src/routes/Index.svelte
@@ -2,16 +2,24 @@
     import { Link } from "svelte-routing";
 </script>
 
-<div class="min-h-screen flex items-center bg-gradient-to-b from-[#89DDFC] via-[#09A6F3] to-[#1363DF]">
+<div
+    class="min-h-screen flex items-center bg-gradient-to-b from-[#89DDFC] via-[#09A6F3] to-[#1363DF]"
+>
     <div class="w-full px-10">
         <div class="translate-y-10">
             <p class="font-semibold text-[32px] text-white">Selamat Datang</p>
             <p class="font-medium text-[20px] text-white">HaloMat App</p>
-            <button class="py-2 bg-[#1363DF] w-full text-white rounded mt-10">
-                <p class="font-medium text-[18px]">Register</p>
-            </button>
+            <Link to="/register">
+                <button
+                    class="py-2 bg-[#1363DF] w-full text-white rounded mt-10"
+                >
+                    <p class="font-medium text-[18px]">Register</p>
+                </button>
+            </Link>
             <Link to="/login">
-                <button class="py-2 bg-[#F3F3F3] w-full text-[#5A5A5A] rounded mt-4">
+                <button
+                    class="py-2 bg-[#F3F3F3] w-full text-[#5A5A5A] rounded mt-4"
+                >
                     <p class="font-medium text-[18px]">Login</p>
                 </button>
             </Link>
@@ -20,5 +28,4 @@
             <p class="text-center">Powered by @RahmatLover</p>
         </div>
     </div>
-
 </div>
